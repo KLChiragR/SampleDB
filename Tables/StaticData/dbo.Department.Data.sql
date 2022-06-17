366b9eb9809980756742dd2ef243f7ab9639b7a1
Chirag Rathod <Chirag.Rathod@KALELOGISTICS.IN>
2022-06-17 16:11:52 +0530

    Add data of the department table

---
+++ Tables/StaticData/dbo.Department.Data.sql
@@ -0,0 +1,7 @@
+﻿SET IDENTITY_INSERT dbo.Department ON
+GO
+INSERT dbo.Department(DepartmentId, DepartmentName, DepartmentCode) VALUES (1, 'Administrator', 'Adm');
+INSERT dbo.Department(DepartmentId, DepartmentName, DepartmentCode) VALUES (2, 'Finance', 'Fin');
+GO
+SET IDENTITY_INSERT dbo.Department OFF
+GO
