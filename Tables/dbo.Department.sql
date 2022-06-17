25290ddc29eb057081eae97374a387df100c57a5
Chirag Rathod <Chirag.Rathod@KALELOGISTICS.IN>
2022-06-17 15:19:04 +0530

    Intial commit

---
+++ Tables/dbo.Department.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Department] (
+  [DepartmentId] [int] IDENTITY,
+  [DepartmentName] [varchar](100) NULL,
+  CONSTRAINT [PK_Department] PRIMARY KEY CLUSTERED ([DepartmentId])
+)
+ON [PRIMARY]
+GO
